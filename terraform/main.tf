e3e401db7f6236e86aa58c4fa380ccc5de4372c9
desilva <desilva@ubiwhere.com>
2023-04-08 23:26:50 +0100

    Add multi worker setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,5 @@
 terraform {
   required_version = ">= 0.13.0"
-  # Initialize the local backend
-  required_providers {
-    # https://github.com/Telmate/terraform-provider-proxmox
-    proxmox = {
-      source  = "telmate/proxmox"
-      version = "2.9.3"
-    }
-
-  }
   backend "s3" {
     bucket                      = "terraform-states"
     key                         = "services-dsilva-dev.tfstate"
@@ -19,7 +10,16 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
   }
-
+  required_providers {
+    # https://github.com/Telmate/terraform-provider-proxmox
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "2.9.3"
+    }
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+    }
+  }
 }
 
 provider "proxmox" {
@@ -37,10 +37,11 @@ provider "proxmox" {
 }
 
 
-# Control plane node
-resource "proxmox_vm_qemu" "k3s-control-plane-node" {
-  name        = "k3s-master"
-  desc        = "Kubernetes control plane node for services.dsilva.dev"
+resource "proxmox_vm_qemu" "k3s-server-nodes" {
+
+  count       = 1
+  name        = "${var.cluster_name}-k3s-server-node-${count.index + 1}"
+  desc        = "Kubernetes server node ${count.index + 1} for ${var.cluster_name}"
   target_node = "proxmox"
 
   # Hardware configuration
@@ -58,7 +59,7 @@ resource "proxmox_vm_qemu" "k3s-control-plane-node" {
 
   os_type         = "cloud-init"
   ipconfig0       = "ip=dhcp" # auto-assign a IP address for the machine
-  nameserver      = var.nameserver
+  nameserver      = "8.8.8.8"
   ciuser          = var.ciuser
   sshkeys         = var.ssh_keys
   ssh_user        = var.ciuser
@@ -73,76 +74,64 @@ resource "proxmox_vm_qemu" "k3s-control-plane-node" {
     port        = self.ssh_port
   }
 
-
-  provisioner "local-exec" {
-    command = <<-EOT
-          echo "${self.ssh_private_key}" > privkey
-          chmod 600 privkey
-          echo ${self.ssh_user}
-          echo ${self.ssh_host}
-          k3sup install --ip ${self.ssh_host} --ssh-key privkey --user ${self.ssh_user} --k3s-version v1.26.3+k3s1
-        EOT
-  }
-}
-
-# Create the worker nodes
-resource "proxmox_vm_qemu" "k3s-worker-nodes" {
-
-  # Only start creating worker nodes after control plane
-  # node is ready
-  depends_on = [
-    proxmox_vm_qemu.k3s-control-plane-node,
-  ]
-
-  name        = "k3s-worker-1"
-  desc        = "Kubernetes worker node 1 for services.dsilva.dev"
-  target_node = "proxmox"
-
-  # Hardware configuration
-  agent   = 1
-  clone   = "ubuntu-server-22"
-  cores   = var.cores
-  memory  = var.memory
-  sockets = 1
-  cpu     = "host"
-  disk {
-    storage = "local"
-    type    = "virtio"
-    size    = var.disk_size
-  }
-
-  os_type         = "cloud-init"
-  ipconfig0       = "ip=dhcp" # auto-assign a IP address for the machine
-  nameserver      = var.nameserver
-  ciuser          = var.ciuser
-  sshkeys         = var.ssh_keys
-  ssh_user        = var.ciuser
-  ssh_private_key = var.ssh_private_key
-
-  # Specify connection variables for remote execution
-  connection {
-    type        = "ssh"
-    host        = self.ssh_host # Auto-assigned ip address
-    user        = self.ssh_user
-    private_key = self.ssh_private_key
-    port        = self.ssh_port
-  }
-
-  # Create the k3s cluster
+  # Start the cluster
   provisioner "local-exec" {
     command = <<-EOT
       echo "${self.ssh_private_key}" > privkey
       chmod 600 privkey
-      echo ${self.ssh_host}
-      echo ${proxmox_vm_qemu.k3s-control-plane-node.ssh_host}
-      echo ${self.ssh_user}
-
-      k3sup join --ip ${self.ssh_host} --server-ip=${proxmox_vm_qemu.k3s-control-plane-node.ssh_host} --ssh-key privkey --user ${self.ssh_user} --k3s-version v1.26.3+k3s1
-    EOT
-
+      k3sup install --ip ${self.ssh_host} --user ${self.ssh_user} --ssh-key privkey --k3s-version ${var.k3s_version}
+      EOT
   }
+
 }
 
 
+resource "proxmox_vm_qemu" "k3s-worker-nodes" {
 
+  count       = var.worker_node_count
+  name        = "${var.cluster_name}-k3s-worker-${count.index + 1}"
+  desc        = "Kubernetes worker node ${count.index + 1} for ${var.cluster_name}"
+  target_node = "proxmox"
+
+  # Hardware configuration
+  agent   = 1
+  clone   = "ubuntu-server-22"
+  cores   = var.cores
+  memory  = var.memory
+  sockets = 1
+  cpu     = "host"
+  disk {
+    storage = "local"
+    type    = "virtio"
+    size    = var.disk_size
+  }
+
+  os_type         = "cloud-init"
+  ipconfig0       = "ip=dhcp" # auto-assign a IP address for the machine
+  nameserver      = "8.8.8.8"
+  ciuser          = var.ciuser
+  sshkeys         = var.ssh_keys
+  ssh_user        = var.ciuser
+  ssh_private_key = var.ssh_private_key
+
+  # Specify connection variables for remote execution
+  connection {
+    type        = "ssh"
+    host        = self.ssh_host # Auto-assigned ip address
+    user        = self.ssh_user
+    private_key = self.ssh_private_key
+    port        = self.ssh_port
+  }
+
+  # Join the cluster with k3sup
+  provisioner "local-exec" {
+    command = <<-EOT
+      echo "${self.ssh_private_key}" > privkey
+      chmod 600 privkey
+      k3sup join --ip ${self.ssh_host} --server-ip=${proxmox_vm_qemu.k3s-server-nodes[0].ssh_host} --ssh-key privkey --user ${self.ssh_user} --k3s-version ${var.k3s_version}
+    EOT
+
+  }
+
+}
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,40 +1,37 @@
+# Kubernetes cluster configuration
+variable "worker_node_count" {
+  description = "The number of worker nodes in the kubernetes cluster."
+  default     = 2
+}
+variable "cluster_name" {
+  type    = string
+  default = "services"
+}
+variable "k3s_version" {
+  description = "The version of k3s"
+  type        = string
+  default     = "v1.26.3+k3s1"
+}
 
-# Define variables for connection with proxmox
-# server
-variable "proxmox_api_url" {
-  type = string
+variable "proxmox_api_token_secret" {
+  type      = string
+  sensitive = true
 }
 variable "proxmox_api_token_id" {
   type      = string
   sensitive = true
 }
-variable "proxmox_api_token_secret" {
+variable "proxmox_api_url" {
   type      = string
   sensitive = true
 }
-variable "worker_node_count" {
-  description = "The number of worker nodes in the kubernetes cluster."
-  default     = 2
-}
-variable "ciuser" {
-  description = "The name of the default user that should be created"
-  type        = string
-}
 
-variable "ssh_keys" {
-  description = "The SSH key to add to the VM"
-  type        = string
-}
-variable "ssh_private_key" {
-  description = "The private SSH key for terraform to SSH into the machine"
-  sensitive   = true
-  type        = string
-}
-variable "nameserver" {
-  description = "The name server for the VM"
-  type        = string
-  default     = "8.8.8.8"
-}
+
+
+
+
+
+# Hardware configuration for kubernetes nodes
 variable "disk_size" {
   description = "The disk size. E.g. '50G'"
   type        = string
@@ -50,3 +47,18 @@ variable "cores" {
   type        = number
   default     = 1
 }
+# SSH configuration
+variable "ciuser" {
+  description = "The name of the default user that should be created"
+  type        = string
+}
+variable "ssh_keys" {
+  description = "The SSH key to add to the VM"
+  type        = string
+}
+variable "ssh_private_key" {
+  description = "The private SSH key for terraform to SSH into the machine"
+  sensitive   = true
+  type        = string
+}
+
